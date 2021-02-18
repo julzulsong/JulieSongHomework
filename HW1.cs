4882bc9566d3d16c88f5c2141954fd6fc7060e23
juliesong <78477247+julzulsong@users.noreply.github.com>
2021-02-18 15:31:27 +0900

    Create HW1.cs
    Uploading homwork for wee2 (loop)

---
+++ HW1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HW1 : MonoBehaviour
+{
+    public int total;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 position = Vector3.zero;
+        for(int i = 0; i < total; i++)
+        {
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            position.x = i * 4f;
+            sphere.transform.position = position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
