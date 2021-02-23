8d9a64af20c9423e5c23dc6e500bfd02be6ab740
juliesong <78477247+julzulsong@users.noreply.github.com>
2021-02-24 00:54:28 +0900

    HW4_JULIE

---
+++ HW4.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HW4 : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    public Vector3[] positions;
+    private int index = 0;
+    public float moveSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = positions[index];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        transform.position = newPosition;
+        float sqDistance = (transform.position - targetPosition).sqrMagnitude;
+        if(sqDistance < .05f){
+            transform.position = targetPosition;
+            targetPosition = positions[index];
+            index++;
+            index %= positions.Length;
+        }
+    }
+}
---
+++ HW4_JUL.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class HW4_JUL : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody rb;
+
+    private Vector3 targetPosition;
+    public Vector3[] targetPositions;
+    private int index = 8;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        getTargetPosition();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+
+        Vector3 newPosition = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        rb.MovePosition(newPosition);
+
+        if((transform.position - targetPosition).sqrMagnitude < .05f){
+            Debug.Log("Here!");
+        
+
+        }
+        
+    }
+
+    void getTargetPosition(){
+        targetPosition = targetPositions[index];
+        targetPosition.y = transform.localScale.y / 2;
+        index++;
+        index %= targetPositions.Length;
+    }
+}
